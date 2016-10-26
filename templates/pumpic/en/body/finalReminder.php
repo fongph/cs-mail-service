518bdbbc9b5de3b47affe5cb1d5cd1b351473b0d
lakkie <k.lena@dizboard.com>
2016-10-26 12:58:20 +0300

    SD-2625 added new emails "finalReminder" and "unfinishedPurchaseNotigication"

---
+++ templates/pumpic/en/body/finalReminder.php
@@ -0,0 +1,22 @@
+<?php $this->layout('layout/default', ['title' => $title, 'style' => $style, 'group' => $group]) ?>
+
+<?php $this->start('page') ?>
+    <p style="<?= $style['paragraph'] ?>">
+        We wanted to remind you one last time that it’s been over 24 hours since you haven’t completed your purchase at Pumpic.
+        <br>
+        If you experienced any problems, please, let us know about it
+    </p>
+    <a class="btn" href="<?= $this->analyticsLink('http://pumpic.com/faq.html', ['source' => 'pumpic', 'medium' => 'email', 'campaign' => 'abandoned_cart', 'content' => 'notification2']) ?>" style="<?= $style['button'] ?>">
+        I need help
+    </a>
+    <p style="<?= $style['paragraph'] ?>">
+        In case you have finished the purchase, you can ignore this email. </p>
+
+    <a class="btn" href="<?= $this->analyticsLink(' https://cp.pumpic.com', ['source' => 'pumpic', 'medium' => 'email', 'campaign' => 'abandoned_cart', 'content' => 'notification2']) ?>" style="<?= $style['button'] ?>">
+        Go to Control Panel
+        </a>
+<?php $this->stop() ?>
+<?php $this->start('footer') ?>
+    Warmly, <br>
+    Pumpic Team
+<?php $this->stop() ?>
---
+++ templates/pumpic/en/body/unfinishedPurchaseNotification.php
@@ -0,0 +1,25 @@
+<?php $this->layout('layout/default', ['title' => $title, 'style' => $style, 'group' => $group]) ?>
+
+<?php $this->start('page') ?>
+<p style="<?= $style['paragraph'] ?>">
+    You have received this notification because you didn’t complete your purchase on Pumpic website. <br />
+    If you aborted the purchase due to technical reasons, you can go back to the Store and start a new purchase process.
+</p>
+<a class="btn" href="<?= $this->analyticsLink('http://pumpic.com/store.html', ['source' => 'pumpic', 'medium' => 'email', 'campaign' => 'abandoned_cart', 'content' => 'notification1']) ?>" style="<?= $style['button'] ?>">
+    Go to the Store
+</a>
+<p style="<?= $style['paragraph'] ?>">
+If you still hesitate or have any questions about Pumpic product, contact Support and we will gladly help you to sort out all the peculiarities of our product!
+</p>
+<a class="btn" href="<?= $this->analyticsLink('http://pumpic.com/faq.html', ['source' => 'pumpic', 'medium' => 'email', 'campaign' => 'abandoned_cart', 'content' => 'notification1']) ?>" style="<?= $style['button'] ?>">
+    I need help
+</a>
+<p style="<?= $style['paragraph'] ?>">
+In case you have finished the purchase and received this email by mistake, please, press  <a style="<?= $style['textLink'] ?>" href="<?= $this->analyticsLink('https://cp.pumpic.com', ['source' => 'pumpic', 'medium' => 'email', 'campaign' => 'abandoned_cart', 'content' => 'notification1']) ?>" >
+        <strong style="<?= $style['strong'] ?>">I already completed a purchase</strong></a>
+</p>
+<?php $this->stop() ?>
+<?php $this->start('footer') ?>
+Warmly, <br>
+Pumpic Team
+<?php $this->stop() ?>
