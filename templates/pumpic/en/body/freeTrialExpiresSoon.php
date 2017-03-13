b74305812ee65a5f169ef7dac3a59a42aca01b78
lakie <k.lena@dizboard.com>
2017-03-13 14:39:48 +0200

    SD-3032 added changes in free trial expires email

--- templates/pumpic/en/body/freeTrialExpiresSoon.php
+++ templates/pumpic/en/body/freeTrialExpiresSoon.php
@@ -14,8 +14,16 @@
     <br>We hope you enjoy the ultimate monitoring features of the Pumpic app.</p>
 
 <p style="<?= $style['paragraph'] ?>">
-    Please note that your trial is about to expire in <strong style="<?= $style['strong'] ?>"><?= $params['numberDays'] ?> day<?php if ($params['numberDays'] > 1): ?>s<?php endif; ?></strong>!
-    You can <a href="<?= $this->analyticsLink('https://pumpic.com/store.html', ['term' => 'store']) ?>" style="<?= $style['textLink'] ?>">subscribe</a> to one of our plans right now and continue using the app without interruption.
+    Please, note that your trial is about to expire in <strong style="<?= $style['strong'] ?>"><?= $params['numberDays'] ?> day<?php if ($params['numberDays'] > 1): ?>s<?php endif; ?></strong>!
+    You can
+    <a
+            href="<?= $this->analyticsLink('https://pumpic.com/store.html', ['source' => 'system', 'medium' => 'system-email', 'term' => 'store', 'campaign' => 'free-trial-expires-soon']) ?>"
+            style="<?= $style['textLink'] ?>">
+        purchase
+    </a>
+    one of our subscription plans right now and continue using the app without the need to reassign the device or any
+    other delay.
+    At that, all previously collected data will remain available in the Control Panel.
 </p>
 
 <i style="border: 0;
